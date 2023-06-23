0fc1dd03f2b38b7cd06e3263e3939453ddf93df4
jclepelch <60457193+jclepelch@users.noreply.github.com>
2023-06-23 13:36:00 -0700

    Update ExampleCodes/FFTW/Basic/main.cpp
    Co-authored-by: Andy Nonaka <AJNonaka@lbl.gov>

--- ExampleCodes/FFTW/Basic/main.cpp
+++ ExampleCodes/FFTW/Basic/main.cpp
@@ -121,7 +121,10 @@ int main (int argc, char* argv[])
             Real y = (j+0.5) * dx[1];
             Real z = (AMREX_SPACEDIM==3) ? (k+0.5) * dx[2] : 0.;
         Real rsquared = (( x-0.5)*(x-0.5)+(y-0.5)*(y-0.5)+(z-0.5)*(z-0.5))/0.01;
-            phi_ptr(i,j,k) = sin(2*M_PI*x + omega)*sin(2*M_PI*y + omega)*sin(2*M_PI*z + omega);
+            phi_ptr(i,j,k) = std::sin(2*M_PI*x + omega)*std::sin(2*M_PI*y + omega);
+            if (AMREX_SPACEDIM == 3) {
+                phi_ptr *= std::sin(2*M_PI*z + omega);
+            }
         });
     }
 
