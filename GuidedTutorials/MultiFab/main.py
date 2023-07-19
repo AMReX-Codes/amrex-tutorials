e61d3603fb8ef18d131042a458cf220ca74c0a98
Edoardo Zoni <ezoni@lbl.gov>
2023-07-19 15:06:05 -0700

    Replace `for` loop with NumPy operations

--- GuidedTutorials/MultiFab/main.py
+++ GuidedTutorials/MultiFab/main.py
@@ -58,23 +58,26 @@ dx = geom.data().CellSize() # dx[0]=dx dx[1]=dy dx[2]=dz
 mf_val = 0.
 for mfi in mf:
     bx = mfi.validbox()
-    mf_array = mf.array(mfi)
-    for i, j, k in bx:
-        x = (i+0.5) * dx[0]
-        y = (j+0.5) * dx[1]
-        z = (k+0.5) * dx[2]
-        r_squared = ((x-0.5)*(x-0.5)+(y-0.5)*(y-0.5)+(z-0.5)*(z-0.5))/0.01
-        mf_array[i,j,k,0] = 1.0 + np.exp(-r_squared)
+    # mf_array is indexed in reversed order (z,y,x) and indices are local,
+    # that is, indices range from 0 to the box size
+    mf_array = np.array(mf.array(mfi), copy=False)
+    x = (np.arange(bx.small_end[0], bx.big_end[0])+0.5)*dx[0]
+    y = (np.arange(bx.small_end[1], bx.big_end[1])+0.5)*dx[1]
+    z = (np.arange(bx.small_end[2], bx.big_end[2])+0.5)*dx[2]
+    v = (x[np.newaxis,np.newaxis,:]*10
+       + y[np.newaxis,:,np.newaxis]*100
+       + z[:,np.newaxis,np.newaxis]*1000)
+    mf_array = 1. + np.exp(-v)
     # Check that mf_array has been filled correctly for a given set of indices
     ii = bx.small_end[0]
     jj = bx.small_end[1] 
     kk = bx.small_end[2]
-    xx = (ii+0.5) * dx[0]
-    yy = (jj+0.5) * dx[1]
-    zz = (kk+0.5) * dx[2]
-    rr_squared = ((xx-0.5)*(xx-0.5)+(yy-0.5)*(yy-0.5)+(zz-0.5)*(zz-0.5))/0.01
-    mf_refval = 1.0 + np.exp(-rr_squared)
-    assert(mf_array[i,j,k,0] == mf_refval)
+    xx = (ii+0.5)*dx[0]
+    yy = (jj+0.5)*dx[1]
+    zz = (kk+0.5)*dx[2]
+    vv = xx*10 + yy*100 + zz*1000
+    mf_refval = 1.0 + np.exp(-vv)
+    assert(mf_array[0,0,0] == mf_refval)
 
 # TODO How do we write plotfiles?
 # Plot MultiFab data
