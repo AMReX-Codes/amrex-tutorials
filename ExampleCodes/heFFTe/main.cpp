cff111e1ab7b0a63f44d049263dc689771bd5230
Andy Nonaka <AJNonaka@lbl.gov>
2023-08-30 14:28:35 -0700

    simplify tutorial code

--- ExampleCodes/heFFTe/main.cpp
+++ ExampleCodes/heFFTe/main.cpp
@@ -1,8 +1,8 @@
 #include <AMReX.H>
 #include <AMReX_MultiFab.H>
-#include <AMReX_LayoutData.H>
 #include <AMReX_ParmParse.H>
 #include <AMReX_GpuComplex.H>
+#include <AMReX_PlotFileUtil.H>
 
 #include <heffte.h>
 
@@ -18,14 +18,12 @@ int main (int argc, char* argv[])
     Geometry geom;
     BoxArray ba;
     DistributionMapping dm;
-    IntVect nghost;
     {
         ParmParse pp;
         IntVect n_cell;
         IntVect max_grid_size;
         pp.get("n_cell", n_cell);
         pp.get("max_grid_size", max_grid_size);
-        pp.get("nghost", nghost);
 
         Box domain(IntVect(0),n_cell-IntVect(1));
         RealBox rb({0.,0.,0.},{1.,1.,1.});
@@ -38,9 +36,26 @@ int main (int argc, char* argv[])
         dm.define(ba);
     }
 
-    MultiFab orig_field(ba,dm,1,0,MFInfo().SetArena(The_Device_Arena()));
-    for (MFIter mfi(orig_field); mfi.isValid(); ++mfi) {
-        Array4<Real> const& fab = orig_field.array(mfi);
+    Box my_domain;
+    int my_boxid;
+    {
+        for (int i = 0; i < ba.size(); ++i) {
+            Box b = ba[i];
+            // each MPI rank has its own my_domain Box and my_boxid ID
+            if (ParallelDescriptor::MyProc() == dm[i]) {
+                my_domain = b;
+                my_boxid = i;
+            }
+        }
+    }
+    
+    MultiFab real_field(ba,dm,1,0,MFInfo().SetArena(The_Device_Arena()));
+
+    // check to make sure each MPI rank has exactly 1 box
+    AMREX_ALWAYS_ASSERT_WITH_MESSAGE(real_field.local_size() == 1, "Must have one Box per process");
+    
+    for (MFIter mfi(real_field); mfi.isValid(); ++mfi) {
+        Array4<Real> const& fab = real_field.array(mfi);
         amrex::ParallelForRNG(mfi.fabbox(),
         [=] AMREX_GPU_DEVICE (int i, int j, int k, RandomEngine const& engine) noexcept
         {
@@ -48,41 +63,18 @@ int main (int argc, char* argv[])
         });
     }
 
-    AMREX_ALWAYS_ASSERT_WITH_MESSAGE(orig_field.local_size() == 1, "Must have one Box per process");
+    Box r_local_box = my_domain;
 
-    BoxArray real_ba;
-    Box my_domain;
-    int my_boxid;
-    {
-        BoxList bl;
-        bl.reserve(ba.size());
-        for (int i = 0; i < ba.size(); ++i) {
-            Box b = ba[i];
-            for (int idim = 0; idim < AMREX_SPACEDIM; ++idim) {
-                if (b.smallEnd(idim) == geom.Domain().smallEnd(idim)) {
-                    b.growLo(idim,nghost[idim]);
-                }
-                if (b.bigEnd(idim) == geom.Domain().bigEnd(idim)) {
-                    b.growHi(idim,nghost[idim]);
-                }
-            }
-            bl.push_back(b);
-            if (ParallelDescriptor::MyProc() == dm[i]) {
-                my_domain = b;
-                my_boxid = i;
-            }
-        }
-        real_ba.define(std::move(bl));
-    }
-    MultiFab real_field(real_ba,dm,1,0,MFInfo().SetArena(The_Device_Arena()));
-    real_field.setVal(0.0+ParallelDescriptor::MyProc()); // touch the memory
-
-    Box r_local_box = amrex::shift(my_domain, nghost);
+    Print() << "r_local_box " << r_local_box << std::endl;
+    
     Box c_local_box = amrex::coarsen(r_local_box, IntVect(2,1,1));
+
+    Print() << "c_local_box " << c_local_box << std::endl;
+    
     if (c_local_box.bigEnd(0) * 2 == r_local_box.bigEnd(0)) {
         c_local_box.setBig(0,c_local_box.bigEnd(0)-1);// to avoid overlap
     }
-    if (my_domain.bigEnd(0) == geom.Domain().bigEnd(0) + nghost[0]) {
+    if (my_domain.bigEnd(0) == geom.Domain().bigEnd(0)) {
         c_local_box.growHi(0,1);
     }
 
@@ -110,7 +102,6 @@ int main (int argc, char* argv[])
 #endif
 
 #endif
-
         ({{r_local_box.smallEnd(0),r_local_box.smallEnd(1),r_local_box.smallEnd(2)},
           {r_local_box.bigEnd(0)  ,r_local_box.bigEnd(1)  ,r_local_box.bigEnd(2)}},
          {{c_local_box.smallEnd(0),c_local_box.smallEnd(1),c_local_box.smallEnd(2)},
@@ -120,11 +111,11 @@ int main (int argc, char* argv[])
     using heffte_complex = typename heffte::fft_output<Real>::type;
     heffte_complex* spectral_data = (heffte_complex*) spectral_field.dataPtr();
 
-    fft.forward(real_field[my_boxid].dataPtr(), spectral_data);
-    fft.backward(spectral_data, real_field[my_boxid].dataPtr());
-
-    ParallelDescriptor::Barrier();
-
+    Real time = 0.;
+    int step = 0;
+    
+    WriteSingleLevelPlotfile("plt_in", real_field, {"phi"}, geom, time, step);
+    
     { BL_PROFILE("HEFFTE-total");
     {
         BL_PROFILE("ForwardTransform");
@@ -136,6 +127,10 @@ int main (int argc, char* argv[])
         fft.backward(spectral_data, real_field[my_boxid].dataPtr());
     }
     }
+    
+    real_field.mult(1./(64.*64.*64.));
+
+    WriteSingleLevelPlotfile("plt_out", real_field, {"phi"}, geom, time, step);
 
     } amrex::Finalize();
 }
