e1cd531cf0319b6d0617a55202586b7ff782fce7
Andy Nonaka <AJNonaka@lbl.gov>
2023-08-31 16:12:16 -0700

    unpacking and shifting diagnostics work in early testing

--- ExampleCodes/heFFTe/main.cpp
+++ ExampleCodes/heFFTe/main.cpp
@@ -72,17 +72,9 @@ int main (int argc, char* argv[])
     // Make a single box that is the entire domain
     Box domain(dom_lo, dom_hi);
 
-    // Box for fft data
-    Box domain_fft = amrex::coarsen(domain, IntVect(AMREX_D_DECL(2,1,1)));
-    if (domain_fft.bigEnd(0) * 2 == domain.bigEnd(0)) {
-        // this avoids overlap for the cases when one or more r_local_box's
-        // have an even cell index in the hi-x cell
-        domain_fft.setBig(0,domain_fft.bigEnd(0)-1);
-    }
-    domain_fft.growHi(0,1);
-
     // number of points in the domain
     long npts = domain.numPts();
+    Real sqrtnpts = std::sqrt(npts);
 
     IntVect max_grid_size(AMREX_D_DECL(max_grid_size_x,max_grid_size_y,max_grid_size_z));
 
@@ -94,7 +86,6 @@ int main (int argc, char* argv[])
     Array<int,AMREX_SPACEDIM> is_periodic{AMREX_D_DECL(1,1,1)};
 
     Geometry geom(domain, real_box, CoordSys::cartesian, is_periodic);
-    Geometry geom_fft(domain_fft, real_box, CoordSys::cartesian, is_periodic);
 
     // extract dx from the geometry object
     GpuArray<Real,AMREX_SPACEDIM> dx = geom.CellSizeArray();
@@ -116,9 +107,12 @@ int main (int argc, char* argv[])
     Real omega = M_PI/2.0;
     
     for (MFIter mfi(real_field); mfi.isValid(); ++mfi) {
+
         Array4<Real> const& fab = real_field.array(mfi);
-        amrex::ParallelForRNG(mfi.fabbox(),
-        [=] AMREX_GPU_DEVICE (int i, int j, int k, RandomEngine const& engine) noexcept
+
+        const Box& bx = mfi.fabbox();
+
+        amrex::ParallelForRNG(bx, [=] AMREX_GPU_DEVICE (int i, int j, int k, RandomEngine const& engine) noexcept
         {
             
             // **********************************
@@ -143,21 +137,113 @@ int main (int argc, char* argv[])
         });
     }
 
-
-    BoxArray fft_ba;
-    Box my_domain;
-    int my_boxid;
+    // since there is 1 MPI rank per box, here each MPI rank obtains its local box and the associated boxid
+    Box local_box;
+    int local_boxid;
     {
         BoxList bl;
         bl.reserve(ba.size());
 
         for (int i = 0; i < ba.size(); ++i) {
             Box b = ba[i];
-            // each MPI rank has its own my_domain Box and my_boxid ID
+            // each MPI rank has its own local_box Box and local_boxid ID
             if (ParallelDescriptor::MyProc() == dm[i]) {
-                my_domain = b;
-                my_boxid = i;
+                local_box = b;
+                local_boxid = i;
             }
+            
+        }
+    }
+
+    // now each MPI rank works on its own box
+    Box c_local_box = amrex::coarsen(local_box, IntVect(AMREX_D_DECL(2,1,1)));
+
+    if (c_local_box.bigEnd(0) * 2 == local_box.bigEnd(0)) {
+        // this avoids overlap for the cases when one or more local_box's
+        // have an even cell index in the hi-x cell
+        c_local_box.setBig(0,c_local_box.bigEnd(0)-1);
+    }
+    if (local_box.bigEnd(0) == geom.Domain().bigEnd(0)) {
+        // increase the size of boxes touching the hi-x domain by 1 in x
+        // this is an (Nx x Ny x Nz) -> (Nx/2+1 x Ny x Nz) real-to-complex sizing
+        c_local_box.growHi(0,1);
+    }
+
+    BaseFab<GpuComplex<Real> > spectral_field(c_local_box, 1, The_Device_Arena());
+
+#if (AMREX_SPACEDIM==2)
+
+#ifdef AMREX_USE_CUDA
+    heffte::fft2d_r2c<heffte::backend::cufft> fft
+#elif AMREX_USE_HIP
+    heffte::fft2d_r2c<heffte::backend::rocfft> fft
+#else
+    heffte::fft2d_r2c<heffte::backend::fftw> fft
+#endif
+        ({{local_box.smallEnd(0),local_box.smallEnd(1),0},
+          {local_box.bigEnd(0)  ,local_box.bigEnd(1)  ,0}},
+         {{c_local_box.smallEnd(0),c_local_box.smallEnd(1),0},
+          {c_local_box.bigEnd(0)  ,c_local_box.bigEnd(1)  ,0}},
+         0, ParallelDescriptor::Communicator());
+
+#elif (AMREX_SPACEDIM==3)
+
+#ifdef AMREX_USE_CUDA
+    heffte::fft3d_r2c<heffte::backend::cufft> fft
+#elif AMREX_USE_HIP
+    heffte::fft3d_r2c<heffte::backend::rocfft> fft
+#else
+    heffte::fft3d_r2c<heffte::backend::fftw> fft
+#endif
+        ({{local_box.smallEnd(0),local_box.smallEnd(1),local_box.smallEnd(2)},
+          {local_box.bigEnd(0)  ,local_box.bigEnd(1)  ,local_box.bigEnd(2)}},
+         {{c_local_box.smallEnd(0),c_local_box.smallEnd(1),c_local_box.smallEnd(2)},
+          {c_local_box.bigEnd(0)  ,c_local_box.bigEnd(1)  ,c_local_box.bigEnd(2)}},
+         0, ParallelDescriptor::Communicator());
+
+#endif
+
+    using heffte_complex = typename heffte::fft_output<Real>::type;
+    heffte_complex* spectral_data = (heffte_complex*) spectral_field.dataPtr();
+
+    Real time = 0.;
+    int step = 0;
+
+    WriteSingleLevelPlotfile("phi_in", real_field, {"phi"}, geom, time, step);
+
+    { BL_PROFILE("HEFFTE-total");
+        {
+            BL_PROFILE("ForwardTransform");
+            fft.forward(real_field[local_boxid].dataPtr(), spectral_data);
+        }
+
+        // for Poisson equation, implement the 1/k^2 scaling here
+        //
+        //
+        //
+        
+        {
+            BL_PROFILE("BackwardTransform");
+            fft.backward(spectral_data, real_field[local_boxid].dataPtr());
+        }
+    }
+
+    // scale by 1/npts (both forward and inverse need 1/sqrtnpts scaling so I am doing it all here)
+    real_field.mult(1./npts);
+
+    WriteSingleLevelPlotfile("phi_out", real_field, {"phi"}, geom, time, step);
+    
+    // **********************************
+    // diagnostics
+    // **********************************
+
+    BoxArray fft_ba;
+    {
+        BoxList bl;
+        bl.reserve(ba.size());
+
+        for (int i = 0; i < ba.size(); ++i) {
+            Box b = ba[i];
 
             Box r_box = b;
             Box c_box = amrex::coarsen(r_box, IntVect(AMREX_D_DECL(2,1,1)));
@@ -177,113 +263,200 @@ int main (int argc, char* argv[])
         }
         fft_ba.define(std::move(bl));
     }
-    Print() << "fft_ba " << fft_ba << std::endl;
 
     // storage for real, imaginary, magnitude, and phase
     MultiFab fft_data(fft_ba,dm,4,0);
-    fft_data.setVal(666.e66);
-
-    // now each MPI rank works on its own box
-    Box r_local_box = my_domain;
-    Box c_local_box = amrex::coarsen(r_local_box, IntVect(AMREX_D_DECL(2,1,1)));
-
-    if (c_local_box.bigEnd(0) * 2 == r_local_box.bigEnd(0)) {
-        // this avoids overlap for the cases when one or more r_local_box's
-        // have an even cell index in the hi-x cell
-        c_local_box.setBig(0,c_local_box.bigEnd(0)-1);
-    }
-    if (my_domain.bigEnd(0) == geom.Domain().bigEnd(0)) {
-        // increase the size of boxes touching the hi-x domain by 1 in x
-        // this is an (Nx x Ny x Nz) -> (Nx/2+1 x Ny x Nz) real-to-complex sizing
-        c_local_box.growHi(0,1);
-    }
-
-    BaseFab<GpuComplex<Real> > spectral_field(c_local_box, 1, The_Device_Arena());
-
-#if (AMREX_SPACEDIM==2)
-
-#ifdef AMREX_USE_CUDA
-    heffte::fft2d_r2c<heffte::backend::cufft> fft
-#elif AMREX_USE_HIP
-    heffte::fft2d_r2c<heffte::backend::rocfft> fft
-#else
-    heffte::fft2d_r2c<heffte::backend::fftw> fft
-#endif
-        ({{r_local_box.smallEnd(0),r_local_box.smallEnd(1),0},
-          {r_local_box.bigEnd(0)  ,r_local_box.bigEnd(1)  ,0}},
-         {{c_local_box.smallEnd(0),c_local_box.smallEnd(1),0},
-          {c_local_box.bigEnd(0)  ,c_local_box.bigEnd(1)  ,0}},
-         0, ParallelDescriptor::Communicator());
-
-#elif (AMREX_SPACEDIM==3)
-
-#ifdef AMREX_USE_CUDA
-    heffte::fft3d_r2c<heffte::backend::cufft> fft
-#elif AMREX_USE_HIP
-    heffte::fft3d_r2c<heffte::backend::rocfft> fft
-#else
-    heffte::fft3d_r2c<heffte::backend::fftw> fft
-#endif
-        ({{r_local_box.smallEnd(0),r_local_box.smallEnd(1),r_local_box.smallEnd(2)},
-          {r_local_box.bigEnd(0)  ,r_local_box.bigEnd(1)  ,r_local_box.bigEnd(2)}},
-         {{c_local_box.smallEnd(0),c_local_box.smallEnd(1),c_local_box.smallEnd(2)},
-          {c_local_box.bigEnd(0)  ,c_local_box.bigEnd(1)  ,c_local_box.bigEnd(2)}},
-         0, ParallelDescriptor::Communicator());
-
-#endif
-
-    using heffte_complex = typename heffte::fft_output<Real>::type;
-    heffte_complex* spectral_data = (heffte_complex*) spectral_field.dataPtr();
-
-    Real time = 0.;
-    int step = 0;
-
-    WriteSingleLevelPlotfile("plt_in", real_field, {"phi"}, geom, time, step);
-
-    { BL_PROFILE("HEFFTE-total");
-    {
-        BL_PROFILE("ForwardTransform");
-        fft.forward(real_field[my_boxid].dataPtr(), spectral_data);
-    }
-
+    
+    // this copies the fft data into a distributed MultiFab
+    // this MultiFab 
     for (MFIter mfi(fft_data); mfi.isValid(); ++mfi) {
+        
         Array4<Real> const& data = fft_data.array(mfi);
         Array4< GpuComplex<Real> > spectral = spectral_field.array();
-        amrex::ParallelFor(mfi.fabbox(), [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+
+        const Box& bx = mfi.fabbox();
+
+        amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
         {
-            data(i,j,k,0) = spectral(i,j,k).real();
-            data(i,j,k,1) = spectral(i,j,k).imag();
-            data(i,j,k,2) = std::sqrt(spectral(i,j,k).real()*spectral(i,j,k).real() +
-                                      spectral(i,j,k).imag()*spectral(i,j,k).imag());
+            Real re = spectral(i,j,k).real() / sqrtnpts;
+            Real im = spectral(i,j,k).imag() / sqrtnpts;
+            
+            data(i,j,k,0) = re;
+            data(i,j,k,1) = im;
+            data(i,j,k,2) = std::sqrt(re*re + im*im);
 
             // Here we want to store the values of the phase angle
             // Avoid division by zero
-            if (spectral(i,j,k).real() == 0.0) {
-                if (spectral(i,j,k).imag() == 0.0){
+            if (re == 0.0) {
+                if (im == 0.0){
                     data(i,j,k,4) = 0.0;
-                } else if (spectral(i,j,k).imag() > 0.0) {
+                } else if (im > 0.0) {
                     data(i,j,k,3) = M_PI/2.0;
                 } else {
                     data(i,j,k,3) = -M_PI/2.0;
                 }
             } else {
-                data(i,j,k,3) = std::atan(spectral(i,j,k).imag()/spectral(i,j,k).real());
+                data(i,j,k,3) = std::atan(im/re);
             }
         });
     }
 
-    WriteSingleLevelPlotfile("fft_data", fft_data, {"real", "imag", "magitude", "phase"}, geom_fft, time, step);
+    // Box for fft data
+    Box domain_fft = amrex::coarsen(domain, IntVect(AMREX_D_DECL(2,1,1)));
+    if (domain_fft.bigEnd(0) * 2 == domain.bigEnd(0)) {
+        // this avoids overlap for the cases when one or more r_local_box's
+        // have an even cell index in the hi-x cell
+        domain_fft.setBig(0,domain_fft.bigEnd(0)-1);
+    }
+    domain_fft.growHi(0,1);
+
+    Geometry geom_fft(domain_fft, real_box, CoordSys::cartesian, is_periodic);
     
-    {
-        BL_PROFILE("BackwardTransform");
-        fft.backward(spectral_data, real_field[my_boxid].dataPtr());
+    WriteSingleLevelPlotfile("fft_data", fft_data, {"real", "imag", "magitude", "phase"}, geom_fft, time, step);
+
+    // unpack, shift, write to plotfile
+    
+    BoxArray ba_onegrid(domain);
+    DistributionMapping dm_onegrid(ba_onegrid);
+
+    MultiFab fft_data_onegrid(ba_onegrid, dm_onegrid, 4, 0);
+    MultiFab fft_data_onegrid_shifted(ba_onegrid, dm_onegrid, 5, 0);  // put phi in component 0
+    // copy in real and imaginary parts
+    fft_data_onegrid.ParallelCopy(fft_data, 0, 0, 2);
+
+    // copy data to a full-sized MultiFab
+    // this involves copying the complex conjugate from the half-sized field
+    // into the appropriate place in the full MultiFab
+    for (MFIter mfi(fft_data_onegrid); mfi.isValid(); ++mfi) {
+
+        Array4<Real> const& data = fft_data_onegrid.array(mfi);
+
+        const Box& bx = mfi.fabbox();
+
+        ParallelFor(bx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+        /*
+         Unpacking rules:
+
+         For domains from (0,0,0) to (Nx-1,Ny-1,Nz-1)
+
+         For any cells with i index > Nx/2, these values are complex conjugates of the corresponding
+         entry where (Nx-i,Ny-j,Nz-k) UNLESS that index is zero, in which case you use 0.
+
+         e.g. for an 8^3 domain, any cell with i index
+
+         Cell (6,2,3) is complex conjugate of (2,6,5)
+
+         Cell (4,1,0) is complex conjugate of (4,7,0)  (note that the FFT is computed for 0 <= i <= Nx/2)
+        */
+            if (i <= bx.length(0)/2) {
+                // do nothing, data is already here
+            } else {
+                // copy complex conjugate
+                int iloc = bx.length(0)-i;
+                int jloc = 0;
+                int kloc = 0;
+#if (AMREX_SPACEDIM >= 2)
+                jloc = (j == 0) ? 0 : bx.length(1)-j;
+#endif
+#if (AMREX_SPACEDIM == 3)
+                kloc = (k == 0) ? 0 : bx.length(2)-k;
+#endif
+
+                data(i,j,k,0) =  data(iloc,jloc,kloc,0);
+                data(i,j,k,1) = -data(iloc,jloc,kloc,1);
+            }
+
+            Real re = data(i,j,k,0);
+            Real im = data(i,j,k,1);
+
+            data(i,j,k,2) = std::sqrt(re*re + im*im);
+
+            // Here we want to store the values of the phase angle
+            // Avoid division by zero
+            if (re == 0.0) {
+                if (im == 0.0){
+                    data(i,j,k,4) = 0.0;
+                } else if (im > 0.0) {
+                    data(i,j,k,3) = M_PI/2.0;
+                } else {
+                    data(i,j,k,3) = -M_PI/2.0;
+                }
+            } else {
+                data(i,j,k,3) = std::atan(im/re);
+            }            
+
+        });
     }
+
+    // SHIFT DATA
+
+    // zero_avg=0 means set the k=0 value to zero,
+    // otherwise it sets the k=0 value to the average value of the signal in real space
+    int zero_avg = 1;
+
+    // zero k=0 mode
+    if (zero_avg == 1) {
+        for (MFIter mfi(fft_data_onegrid); mfi.isValid(); ++mfi) {
+
+            const Box& bx = mfi.tilebox();
+        
+            Array4<Real> const& data = fft_data_onegrid.array(mfi);
+
+            amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+            {
+                if (i == 0 && j == 0 && k == 0) {
+                    data(i,j,k,0) = 0.;
+                    data(i,j,k,1) = 0.;
+                    data(i,j,k,2) = 0.;
+                    data(i,j,k,3) = 0.;
+                }
+            });
+        }
     }
 
-    // scale by 1/npts
-    real_field.mult(1./npts);
+    // Shift DFT by N/2+1 (pi)
+    for (MFIter mfi(fft_data_onegrid); mfi.isValid(); ++mfi) {
 
-    WriteSingleLevelPlotfile("plt_out", real_field, {"phi"}, geom, time, step);
+        const Box& bx = mfi.tilebox();
 
+        const Array4<Real>& dft = fft_data_onegrid_shifted.array(mfi);
+        const Array4<Real>& dft_temp = fft_data_onegrid.array(mfi);
+
+        int nx = bx.length(0);
+        int nxh = nx/2;
+#if (AMREX_SPACEDIM >= 2)
+        int ny = bx.length(1);
+        int nyh = ny/2;
+#endif
+#if (AMREX_SPACEDIM == 3)
+        int nz = bx.length(2);
+        int nzh = nz/2;
+#endif
+
+        amrex::ParallelFor(bx, [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
+        {
+            int ip = (i+nxh)%nx;
+            int jp = 0;
+            int kp = 0;
+#if (AMREX_SPACEDIM >= 2)
+            jp = (j+nyh)%ny;
+#endif
+#if (AMREX_SPACEDIM == 3)
+            kp = (k+nzh)%nz;
+#endif
+            for (int comp=0; comp<4; ++comp) {
+                dft(ip,jp,kp,comp+1) = dft_temp(i,j,k,comp);
+            }
+        });
+
+    }
+
+    fft_data_onegrid_shifted.ParallelCopy(real_field, 0, 0, 1);
+  
+    WriteSingleLevelPlotfile("plt", fft_data_onegrid_shifted,
+                             {"phi", "phi_dft_real", "phi_dft_imag", "phi_dft_magitude", "phi_dft_phase"},
+                             geom, time, step);
+        
     } amrex::Finalize();
 }
