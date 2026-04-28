d41b6fbf767c36bf9e733d5af708deff7061975c
Weiqun Zhang <weiqunzhang@lbl.gov>
2026-04-28 09:10:22 -0700

    Update to C++20

--- .github/workflows/dependencies/dependencies.sh
+++ .github/workflows/dependencies/dependencies.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 #
-# Copyright 2020 The AMReX Community
+# Copyright 2020-2022 The AMReX Community
 #
 # License: BSD-3-Clause-LBNL
 # Authors: Axel Huebl
 
 set -eu -o pipefail
 
+# `man apt.conf`:
+#   Number of retries to perform. If this is non-zero APT will retry
+#   failed files the given number of times.
+echo 'Acquire::Retries "3";' | sudo tee /etc/apt/apt.conf.d/80-retries
+
 sudo apt-get update
 
 sudo apt-get install -y --no-install-recommends\
     build-essential \
+    libfftw3-dev    \
     g++ gfortran    \
     libopenmpi-dev  \
-    openmpi-bin     \
-    python3         \
-    python3-pip
-
-python3 -m pip install -U pip
-python3 -m pip install -U build packaging setuptools wheel
+    openmpi-bin
